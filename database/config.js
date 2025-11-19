6644148c585f6ab92245fed25892d77ed6f85729
rexz26hff <bg185967@gmail.com>
2025-11-19 14:12:08 +0700

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,7 @@
+//credit By Rexz-Infinity 
+//no hapus credit ketauan hapus hitam//
+
+module.exports = {
+  domain: "http://jonxz.zonapanel.web.id", // Ubah jadi Domain panel Mu !!!
+  port: "4000" // Ubah Jadi Port Panel Mu !!!
+};
